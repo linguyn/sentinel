c4fcda5b84742b238ed533d4f6636ea514a7c7f1
Linh Nguyen <rene@Renes-macbook-air.local>
2026-09-10 18:49:43 -0400

    chore: set up project folder structure

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Observation(BaseModel): 
+    has_key: bool
+    door_locked: bool
+
+app = FastAPI() 
+
+@app.get("/heath")
+def get_health(): 
+    return {"Status": "Running"}
+
+@app.post('/action')
+def send_action(act: Observation): 
+    response = {
+        "has_key": act.has_key, 
+        "action": "move",
+        "position": "12.2 , 3.4, 5.4"
+    }
+
+    return response
