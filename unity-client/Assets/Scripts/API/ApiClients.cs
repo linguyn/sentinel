36e4d351f38adf9db2a577ab525fbff72c6314fe
Linh Nguyen <rene@Renes-macbook-air.local>
2026-09-15 12:51:39 -0400

    chore: optimize ApiClients.cs

--- unity-client/Assets/Scripts/API/ApiClients.cs
+++ unity-client/Assets/Scripts/API/ApiClients.cs
@@ -1,16 +1,13 @@
 ﻿using UnityEngine; 
 using UnityEngine.Networking;
-using System.Threading.Tasks;
-using System;
 
 /*ApiClients: responsible for backend communication*/
 
 public class ApiClients : MonoBehaviour {
 
-    UnityWebRequestAsyncOperation asyncOP; 
     string webURL = "http://127.0.0.1:8000";
 
-    async Task Start() {
+    async Awaitable Start() {
         Observation observation = new Observation {
             has_key = true,
             door_locked = false
@@ -18,24 +15,26 @@ public class ApiClients : MonoBehaviour {
 
         string json = JsonUtility.ToJson(observation); 
 
-        var result = await asyncPostRequest(json);
+        var result = await AsyncPostRequest(json);
         Debug.Log(result); 
     }
 
-    private async Task<string> asyncPostRequest(string json) {
-        UnityWebRequest request = UnityWebRequest.Post(webURL + "/action", json, "application/json");
-        asyncOP = request.SendWebRequest();
-        Awaitable awaitable = Awaitable.FromAsyncOperation(asyncOP);
-        await awaitable; 
+    private async Awaitable<string> AsyncPostRequest(string json) {
+        using(UnityWebRequest request = UnityWebRequest.Post(webURL + "/action", json, "application/json"))
+        {
+            UnityWebRequestAsyncOperation asyncOP = request.SendWebRequest();
+            Awaitable awaitable = Awaitable.FromAsyncOperation(asyncOP);
+            await awaitable; 
 
-        if (request.result != UnityWebRequest.Result.Success) {
-            Debug.Log(request.error);
-            Debug.Log(request.responseCode);
-            return asyncOP.webRequest.downloadHandler.text;
-        } else {
-            Debug.Log(request.responseCode);
-            return asyncOP.webRequest.downloadHandler.text; 
+            if (request.result != UnityWebRequest.Result.Success) {
+                Debug.Log(request.error);
+                Debug.Log(request.responseCode);
+            } else {
+                Debug.Log(request.responseCode); 
+            }
+
+            return request.downloadHandler.text;
         }
-        
     }
+
 }
