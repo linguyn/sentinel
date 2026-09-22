2a25389bf4f6380b071fc4923383f5fa7ee762bf
Linh Nguyen <Linhnguyen952756@gmail.com>
2026-09-22 18:31:44 -0400

    chore: optimize AsyncPostRequest function and change Start() signature

--- unity-client/Assets/Scripts/API/ApiClients.cs
+++ unity-client/Assets/Scripts/API/ApiClients.cs
@@ -10,7 +10,7 @@ public class ApiClients : MonoBehaviour {
     UnityWebRequestAsyncOperation asyncOP; 
     string webURL = "http://127.0.0.1:8000";
 
-    async Task Start() {
+    async Awaitable Start() {
         Observation observation = new Observation {
             has_key = true,
             door_locked = false
@@ -18,11 +18,11 @@ public class ApiClients : MonoBehaviour {
 
         string json = JsonUtility.ToJson(observation); 
 
-        var result = await asyncPostRequest(json);
+        var result = await AsyncPostRequest(json);
         Debug.Log(result); 
     }
 
-    private async Task<string> asyncPostRequest(string json) {
+    private async Task<string> AsyncPostRequest(string json) {
         UnityWebRequest request = UnityWebRequest.Post(webURL + "/action", json, "application/json");
         asyncOP = request.SendWebRequest();
         Awaitable awaitable = Awaitable.FromAsyncOperation(asyncOP);
